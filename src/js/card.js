sschum/tf-2021-demo

create own card component

src/js/card.js
@@ -1,0 +1,11 @@
+Vue.component('card', {
+    template: `
+<div class="card">
+    <div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+        </div>
+    </div>
+</div>
+    `
+})

sschum/tf-2022-demo

src/js/card.js
@@ -1,0 +1,11 @@
+MyVueApp.component('card', {
+    template: `
+<div class="card">
+    <div>
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+        </div>
+    </div>
+</div>
+    `
+})